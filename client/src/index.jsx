16afba85480cfd7fcc4ebbea23ede65557c60d58
Gunpreet Singh <kllrjatt@users.noreply.github.com>
2017-06-25 22:41:47 -0700

    Staging (#9)
    * Update Readme and Schema SQL (#2)
    
    * gitignore issue fixed  (#4)
    
    * Update Readme and Schema SQL
    
    * update gitingore
    
    * created and connected calories, items, and search
    
    * finished search skeleton
    
    * Completed the items component skeleton
    
    * finished total calories skeleton
    
    * added bundle to the git ignore
    
    * attempt 2 to remove bundle.js from commit
    
    * Client Skeleton Created (#7)
    
    * Update Readme and Schema SQL (#2) (#3)
    
    * Fixed an ignore issue  (#5)
    
    * Update Readme and Schema SQL (#2)
    
    * gitignore issue fixed  (#4)
    
    * Update Readme and Schema SQL
    
    * update gitingore
    
    * created and connected calories, items, and search
    
    * finished search skeleton
    
    * Completed the items component skeleton
    
    * finished total calories skeleton
    
    * added bundle to the git ignore
    
    * attempt 2 to remove bundle.js from commit
    
    * client to server get request working
    
    * created first test, and it now passes
    
    * bug fix
    
    * built out test suite
    
    * finished test suite

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -94,4 +94,3 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
+<<<<<<< HEAD
 
 app.get('/foods', function (req, res) {
   var options = {
@@ -95,43 +96,6 @@ app.get('/exercise', function(req, res) {
 
 });
 
-
-app.get('/foods', function (req, res) {
-  var options = {
-    method: 'POST',
-    url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-    headers: {
-      'content-type': 'application/json',
-      'x-app-key': '599e301928d15020ff16d7dbeef77f6f',
-      'x-app-id': '9e058c76'
-    },
-    sort: {
-      order: '_score'
-    },
-    //offset: 0,
-    limit: 5,
-
-    body: {
-      query: req.query.userFood,
-      timezone: 'US/Eastern'
-    },
-    json: true
-  };
-
-  request(options, function (error, response, body) {
-    if (error) {
-      console.log('here in error');
-      throw new Error(error);
-    } else {
-      res.status(200);
-      console.log(body.foods);
-      res.send(body.foods);
-      res.end();
-
-    }
-
-  });
-};
 
 
 const PORT = process.env.PORT || 3000;
