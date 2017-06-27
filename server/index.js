79a54cd50caed4f2a543554e8ea07b520902c6dc
Nick Akey <nick@nickakey.com>
2017-06-27 13:45:51 -0700

    I have built the local strategy, and am trying to make it work with the post route

--- server/index.js
+++ server/index.js
@@ -3,13 +3,84 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var authUtils = require('./authUtils.js');
 const Promise = require('bluebird');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const flash = require('connect-flash');
 Promise.promisifyAll(bcrypt);
 Promise.promisifyAll(authUtils);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const db = require('../database/index.js');
+
+Promise.promisifyAll(db);
 
 var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+app.post('/signup', (req, res)=>{
+  db.isNewUserAsync(req.body.userName)
+  .then(()=>{
+    return bcrypt.genSaltAsync(10);
+  })
+  .then(salt=>{
+    return bcrypt.hashAsync(req.body.password, salt);
+  })
+  .then(hashedPassword=>{
+    console.log('this is a hashed password! ', hashedPassword);
+    return db.insertUserAsync(req.body.userName, hashedPassword);
+  })
+  .then(user=>{
+    res.status(201);
+    res.end();
+  })
+  .catch((err)=>{
+    console.log(err);
+    res.status(404);
+    res.end();
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    db.isExistingUserAsync(username)
+    .then((user)=>{
+      bcrypt.compareAsync(password, user[0].hash)
+      .then(()=>{
+        return db.getHealthHistoryAsync(username);
+      })
+      .then((history)=>{
+        user[0].history = history;
+        return done(null, user, history);
+      });
+    })
+    .catch((err)=>{
+      return done(null, false, {message: err});
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user[0].id);
+});
+
+passport.deserializeUser(function(id, done) {
+  authUtils.getUserByID(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.post('/login', passport.authenticate('local'), 
+  ((req, res)=>{
+    req.flash('info', 'Hi there!');
+    res.status(201);
+    res.json(req.user[0].history);
+    res.end();
+  })
+);
 
 app.get('/foods', function (req, res) {
   var options = {
@@ -25,7 +96,6 @@ app.get('/foods', function (req, res) {
     },
     //offset: 0,
     limit: 5,
-
     body: {
       query: req.query.userFood,
       timezone: 'US/Eastern'
@@ -39,57 +109,10 @@ app.get('/foods', function (req, res) {
       throw new Error(error);
     } else {
       res.status(200);
-      console.log(body.foods);
-      // console.log(body.foods);
       res.send(body.foods);
       res.end();
     }
-  });
-});
 
-app.post('/signup', (req, res)=>{
-  authUtils.isNewUserAsync(req.body.userName)
-  .then(()=>{
-    return bcrypt.genSaltAsync(10)
-  })
-  .then(salt=>{
-    return bcrypt.hashAsync(req.body.password, salt)
-  })
-  .then(hashedPassword=>{
-    return authUtils.storeUserInDBAsync(req.body.userName, hashedPassword) 
-  })
-  .then(user=>{
-    res.status(201);
-    res.end();
-  })
-  .catch((err)=>{
-    res.status(404);
-    res.end()
-  })
-})
-
-app.get('/exercise', function(req, res) {
-  var options = {
-    method: 'POST',
-    url: 'https://trackapi.nutritionix.com/v2/natural/exercise',
-    headers: {
-      'content-type': 'application/json',
-      'x-app-key': '599e301928d15020ff16d7dbeef77f6f',
-      'x-app-id': '9e058c76'
-    },
-    body: {
-      query: req.query.userExercise,
-    },
-    json: true
-  };
-
-  request(options, function (error, response, body) {
-    if (error) {
-      console.log('an error was thrown in fetching exercise from API');
-    } else {
-      console.log(body);
-      res.send(body);
-    }
   });
 });
 
